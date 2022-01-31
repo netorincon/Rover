b61977261ca137f8279aab7843166610bd5926b2
netorincon <netorincon2@hotmail.com>
2022-01-30 23:10:25 -0600

    Creacion del proyecto en STM32CubeIDE
    Se crear el proyecto de STM con la configuracion de interrupcion de los 4 pines para los encoders

---
+++ STM/CM7/Core/Inc/main.h
@@ -0,0 +1,84 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define STLINK_RX_Pin GPIO_PIN_8
+#define STLINK_RX_GPIO_Port GPIOD
+#define STLINK_TX_Pin GPIO_PIN_9
+#define STLINK_TX_GPIO_Port GPIOD
+#define MAC1_Pin GPIO_PIN_4
+#define MAC1_GPIO_Port GPIOB
+#define MAC1_EXTI_IRQn EXTI4_IRQn
+#define MAC2_Pin GPIO_PIN_5
+#define MAC2_GPIO_Port GPIOB
+#define MAC2_EXTI_IRQn EXTI9_5_IRQn
+#define MBC1_Pin GPIO_PIN_6
+#define MBC1_GPIO_Port GPIOB
+#define MBC1_EXTI_IRQn EXTI9_5_IRQn
+#define MBC2_Pin GPIO_PIN_7
+#define MBC2_GPIO_Port GPIOB
+#define MBC2_EXTI_IRQn EXTI9_5_IRQn
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
